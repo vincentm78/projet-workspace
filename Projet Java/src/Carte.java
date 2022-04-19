26c02cb0371b5f46273c4e2ede9b791b324ec630
VincentCode <91540691+vincentm78@users.noreply.github.com>
2022-04-19 22:24:37 +0200

    Update Carte.java

--- Projet Java/src/Carte.java
+++ Projet Java/src/Carte.java
@@ -1,43 +1,112 @@
 import java.util.LinkedList;
-
+//Classe pour tester le contenu des villes
 public class Carte
 {
 	private LinkedList<Ville> carte = new LinkedList<Ville>();
-	private LinkedList<Case> Lieu;
+	
+
+
+	public Carte()
+	{
+		this.carte= new LinkedList<Ville>();
+	}
 	
 	
 	public void creerCarte()
     {
-  	  for (int i=1;i<=9;i++)
+  	  for (int i=0;i<=8;i++)
   	  {
   		  Ville city = new Ville(i);
-  		  carte.add(city);
-  		  for(int j=1;j<=4;j++)
-  		  {
-  		  Case nv = new Case(i,j);
-  		  this.Lieu.add(nv);
-  		  }
+  		  this.carte.add(city);
+  		  
   		  
   	  }
     }
+
 	
-	
-	public void creerPorte()
+	public void affiche()
 	{
-		
+		for(int i=0;i<this.carte.size();i++)
+		{
+		Case[][] nv = this.carte.get(i).getGrille().getCase();
+		for(int j= 0;j<2;j++)
+		{
+			for(int k=0;k<2;k++)
+			{
+			System.out.println(nv[j][k].getLieu());
+			}
+		}
+		}
 	}
 	
-	
-	
-	
-	
-	
-	public void testCarte()
+	public void affichePorte()
 	{
-		
+		for (int i=0;i<this.carte.size();i++)
+		{
+			Case [][] nv = this.carte.get(i).getGrille().getCase();
+			for(int j= 0;j<2;j++)
+			{
+				for(int k=0;k<2;k++)
+				{
+				if(nv[j][k].getPorte()!=null)
+				{
+				System.out.println("|numVilleEntree = " + nv[j][k].getPorte().getVilleEntree()
+				+ " |num Case Entree = " +  nv[j][k].getPorte().getNumCaseEntree()
+				+ " |numVilleSortie = " + nv[j][k].getPorte().getVilleSortie()
+				+ " |numCaseSortie = " + nv[j][k].getPorte().getNumCaseSortie());
+				}
+				}
+			}
 	}
+	}
+		
+		public void afficheTwoPorte()
+		{
+			for (int i=0;i<this.carte.size();i++)
+			{
+				Case [][] nv = this.carte.get(i).getGrille().getCase();
+				for(int j= 0;j<2;j++)
+				{
+					for(int k=0;k<2;k++)
+					{
+					     if(nv[j][k].getTwoPorte()!=null)
+					     {
+					     System.out.println(
+					    		 "|numVilleEntree = " + nv[j][k].getTwoPorte().getPorte1().getVilleEntree() + "|"+
+					    	     "|numCaseEntree = " + nv[j][k].getTwoPorte().getPorte1().getNumCaseEntree() + "|"+
+					    	     "|numVilleSortie = " + nv[j][k].getTwoPorte().getPorte1().getVilleSortie() + "|"+
+					    	     "|numCaseSortie = " + nv[j][k].getTwoPorte().getPorte1().getNumCaseSortie() + "|"
+					    		 );
+					     System.out.println(
+					    		 "|numVilleEntree = " + nv[j][k].getTwoPorte().getPorte2().getVilleEntree() + "|"+
+					    	     "|numCaseEntree = " + nv[j][k].getTwoPorte().getPorte2().getNumCaseEntree() + "|"+
+					    	     "|numVilleSortie = " + nv[j][k].getTwoPorte().getPorte2().getVilleSortie() + "|"+
+					    	     "|numCaseSortie = " + nv[j][k].getTwoPorte().getPorte2().getNumCaseSortie() + "|"
+					    		 );
+					
+					     }
+					
+					}
+				}
+			}
+		}
+		
+		
+		public LinkedList<Ville> getCarte() {
+			return carte;
+		}
+
+
+		public void setCarte(LinkedList<Ville> carte) {
+			this.carte = carte;
+		}
+		
+		
 	
 	
+	 
+	
+
 	
 	
 }
