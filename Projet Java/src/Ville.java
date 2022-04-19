151b32805aba000fdbabbc1107fc73c3cbdfff25
VincentCode <91540691+vincentm78@users.noreply.github.com>
2022-04-19 22:25:16 +0200

    Update Ville.java

--- Projet Java/src/Ville.java
+++ Projet Java/src/Ville.java
@@ -1,47 +1,65 @@
-
-     public class Ville
+public class Ville
 
      {
+
+    	 private Integer numVille;
+    	    private String nom;
+    	    private Grille grille;
+
+
+	public Ville(Integer numVille) {
+  		this.numVille = numVille;
+  		this.nom = donneNomVille(numVille);
+  		Grille nv = new Grille(2,2);
+		nv.donneGrille(numVille);
+		this.grille=nv;
+  	}
+
       
-      private Integer numVille;
-      private String nom;
-     
-
-
-
-     public Ville (Integer numVille)
-     {
-     this.numVille=numVille;
-     this.nom=donneNomVille(numVille);
-     }
-     
-     public void afficheNomVille()
-     {
-    	System.out.println(this.nom);
-     }
-     public void afficheNumVille()
-     {
-    	System.out.println(this.numVille);
-     }
-
-
      public String donneNomVille(Integer x)
      {
+    	 
      switch(x) {
      
-     case 1: return "Madrid";
-     case 2: return "Miami";
-     case 3: return "Shangai";
-     case 4: return "Marseille";
-     case 5: return "Chateauroux";
-     case 6: return "Paris";
-     case 7: return "Tokyo";
-     case 8: return "Sydney";
-     case 9: return "Sao Paulo";
+     case 0: return "Madrid";
+     case 1: return "Miami";
+     case 2: return "Shangai";
+     case 3: return "Marseille";
+     case 4: return "Chateauroux";
+     case 5: return "Paris";
+     case 6: return "Tokyo";
+     case 7: return "Sydney";
+     case 8: return "Sao Paulo";
      }
      return "Ville introuvable";
      
      }
      
      
-     }
+     public Integer getNumVille() {
+ 		return numVille;
+ 	}
+
+ 	public void setNumVille(Integer numVille) {
+ 		this.numVille = numVille;
+ 	}
+
+ 	public String getNom() {
+ 		return nom;
+ 	}
+
+ 	public void setNom(String nom) {
+ 		this.nom = nom;
+ 	}
+ 	public Grille getGrille() {
+		return grille;
+	}
+
+	public void setGrille(Grille grille) {
+		this.grille = grille;
+	}
+
+	
+	
+
+  }
