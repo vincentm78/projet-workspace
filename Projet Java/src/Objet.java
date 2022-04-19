775ba1efeadd619cf61f2f864e20f387e70679f6
VincentCode <91540691+vincentm78@users.noreply.github.com>
2022-04-19 22:29:37 +0200

    Update Objet.java

--- Projet Java/src/Objet.java
+++ Projet Java/src/Objet.java
@@ -1,39 +1,41 @@
-
-public class Objet {
-
+public class Objet
+{
 	private String nom;
-	private Position position;
-	private Personnage détenuPar;
-	private MembreFamille liéA;
-	//private String caractéristiques;
-	private Boolean possédé = false;
+	private String detail;
 	
-	
-	
-	
-	public Objet(String nom, Position lieu, Personnage détenuPar, MembreFamille liéA) {
-		super();
-		this.nom = nom;
-		this.position = lieu;
-		this.détenuPar = détenuPar;
-		this.liéA = liéA;
-	}
-
-
-
-
-	public void trouver()
+	public Objet(String nom,String detail)
 	{
-		this.possédé = true;
+		this.nom=nom;
+		this.detail=detail;
 	}
-
-
-	 
-	//à modifier en fonction du lancement
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	
+	public void afficheNomObjet()
+	{
+	   System.out.println(this.nom);
+	}
+	public void lireDesObjet()
+	{
+		System.out.println(this.detail);
+	}
+	
 	public String toString() {
-		return "Objet [nom=" + nom + ", détenuPar=" + détenuPar + ", liéA=" + liéA + ", possédé="
-				+ possédé + "]";
+		return "Objet [nom=" + nom + ", detail=" + detail + "]";
 	}
+
 	
 	
 }
