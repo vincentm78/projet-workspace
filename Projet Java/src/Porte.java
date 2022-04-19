1bb3a54e9da747e8c89c0054135eb822cf9ca687
VincentCode <91540691+vincentm78@users.noreply.github.com>
2022-04-19 22:26:44 +0200

    Update Porte.java

--- Projet Java/src/Porte.java
+++ Projet Java/src/Porte.java
@@ -1,17 +1,75 @@
-package Carte;
-
 public class Porte
 {
-	private String nomPorte;
-	
-	public Porte(String nomPorte)
-	{
-		this.nomPorte=nomPorte;
-	}
-	public void afficheNomPorte()
-	{
-	  System.out.println(this.nomPorte);
-	}
+	private Integer villeEntree;
+	private Integer numCaseEntree;
+	private Integer villeSortie;
+	private Integer numCaseSortie;
 
 	
+	public Porte()
+	{
+		
+	}
+	
+	//Presence 1 porte
+	public Porte(Integer villeEntree, Integer numCaseEntree, Integer villeSortie,
+			Integer numCaseSortie) {
+		
+		this.villeEntree = villeEntree;
+		this.numCaseEntree = numCaseEntree;
+		this.villeSortie = villeSortie;
+		this.numCaseSortie = numCaseSortie;
+	}
+	
+
+	
+
+
+
+	public Integer getVilleEntree() {
+		return villeEntree;
+	}
+
+
+
+	public void setVilleEntree(Integer villeEntree) {
+		this.villeEntree = villeEntree;
+	}
+
+
+
+	public Integer getNumCaseEntree() {
+		return numCaseEntree;
+	}
+
+
+
+	public void setNumCaseEntree(Integer numCaseEntree) {
+		this.numCaseEntree = numCaseEntree;
+	}
+
+
+
+	public Integer getVilleSortie() {
+		return villeSortie;
+	}
+
+
+
+	public void setVilleSortie(Integer villeSortie) {
+		this.villeSortie = villeSortie;
+	}
+
+
+
+	public Integer getNumCaseSortie() {
+		return numCaseSortie;
+	}
+
+
+
+	public void setNumCaseSortie(Integer numCaseSortie) {
+		this.numCaseSortie = numCaseSortie;
+	}
+
 }
